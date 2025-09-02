332db2b7cb5a8ff8fec745510d3cc8fa2c80f541
isurueranga-98 <isurueranga.0@gmail.com>
2025-09-02 21:15:16 +0530

    refactor: comment out unused logo import and image in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
-import Logo from './ireact.svg';
+// import Logo from './ireact.svg';
 
 const App: FC = () => {
   return (
     <header className="h-screen bg-gradient-to-r from-[#f5f7fa] to-[#c3cfe2] flex items-center justify-center">
       <div className="text-center">
-        <img src={Logo} className="w-96 mx-auto mb-8" alt="iReact Logo" />
+        {/* <img src={Logo} className="w-96 mx-auto mb-8" alt="iReact Logo" /> */}
         <h1 className="text-8xl font-bold text-gray-800">iReactTS</h1>
         <p className="text-2xl text-gray-600 mt-4">
           React 19 + TypeScript 5 + Tailwind CSS 3
